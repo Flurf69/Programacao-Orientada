1225f980090cc54b8f16757edcd9339e94c150d4
Flurf69 <dineves@furb.br>
2024-08-22 21:29:09 -0300

    lista 3 terminada, dar uma arrumada no código do app

--- exercicio3/src/App.java
+++ exercicio3/src/App.java
@@ -16,6 +16,9 @@ public class App {
 
         } while (qtdPessoa <= 4);
 
+        JOptionPane.showMessageDialog(null, "O nome da pessoa 1 é "+ salario[0].getNome() +  salario[0].getSalario() +
+                "O nome da pessoa 2 é " + salario[1].getNome() + salario[1].getSalario() + "O nome da pessoa 3 é " + salario[2].getNome() + salario[2].getSalario() +
+                "O nome da pessoa 4 é " + salario[3].getNome() + salario[3].getSalario()+ "O nome da Pessoa 5 é " + salario[4].getNome() + salario[4].getSalario());
     }
 }
 //teste
--- exercicio3/src/Pessoa.java
+++ exercicio3/src/Pessoa.java
@@ -10,6 +10,9 @@ public class Pessoa {
     }
 
     public void setNome(String nome) {
+        if (nome.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nome está vazio");
+        }
         this.nome = nome;
     }
 
@@ -22,6 +25,29 @@ public class Pessoa {
     }
 
     public void setSalario(double salario) {
+        if (salario <= 0){
+            throw new IllegalArgumentException("numero invalido");
+        }
         this.salario = salario;
+    }
+
+    public double calcularIrfp(){
+        double temp;
+        if(getSalario()<=1903.99 && getSalario()>=2826.65){
+            temp = getSalario()*0.075;
+            this.salario -= temp;
+        }
+        if (getSalario()<=2826.66 && getSalario()>=3751.05){
+            temp = getSalario()*0.15;
+            this.salario -= temp;
+        }
+        if (getSalario()<=3751.06 && getSalario()>=4664.68){
+            temp = getSalario()*0.225;
+            this.salario -= temp;
+        }
+        if (getSalario()>=4664.69){
+            temp = getSalario()*0.275;
+        }
+        return salario;
     }
 }
