5d97458eeb7b7f44d5ca423ee6f0f9b07bba6d36
Flurf69 <dineves@furb.br>
2024-08-28 21:13:34 -0300

    mensagem de erro na propria questão

--- exercicio4/src/numero3/AppQuestao3Ui.java
+++ exercicio4/src/numero3/AppQuestao3Ui.java
@@ -11,14 +11,19 @@ public class AppQuestao3Ui {
         int comprimento2 = Integer.parseInt(JOptionPane.showInputDialog("Qual o comprimento do retangulo?"));
 
         Retangulo r = new Retangulo();
+        try{
+            r.setAltura(altura2);
+            r.setComprimento(comprimento2);
+            int area = r.calcularArea();
+            int perimetro = r.calcularPerimetro();
+            JOptionPane.showMessageDialog(null, "A area do retangulo é " + area);
+            JOptionPane.showMessageDialog(null, "O perimetro do retangulo é " + perimetro);
+        } catch (IllegalArgumentException e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
 
-        r.setAltura(altura2);
-        r.setComprimento(comprimento2);
 
-        int area = r.calcularArea();
-        int perimetro = r.calcularPerimetro();
 
-        JOptionPane.showMessageDialog(null, "A area do retangulo é " + area);
-        JOptionPane.showMessageDialog(null, "O perimetro do retangulo é " + perimetro);
+
     }
 }
