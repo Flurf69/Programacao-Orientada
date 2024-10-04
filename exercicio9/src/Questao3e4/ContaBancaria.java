adc9ef01e1cb66962cc72cc39d69fe27242e7a4e
Flurf69 <dineves@furb.br>
2024-10-03 21:11:09 -0300

    Questão 3 e 4 da lista 9 (completa mas falta teste)

--- exercicio9/src/Questao3e4/ContaBancaria.java
+++ exercicio9/src/Questao3e4/ContaBancaria.java
@@ -1,10 +1,13 @@
 package Questao3e4;
 
+import java.util.ArrayList;
+
 public class ContaBancaria {
 
     private String numero;
     private double saldo;
     private Cliente titular;
+    private ArrayList<Movimento> movimento;
 
     public String getNumero() {
         return numero;
@@ -34,6 +37,14 @@ public class ContaBancaria {
         this.saldo += valor;
     }
 
+    public ArrayList<Movimento> getContas() {
+        return movimento;
+    }
+
+    public void setContas(ArrayList<ContaBancaria> contas) {
+        this.movimento = movimento;
+    }
+
     public void sacar(double valor) {
         if (this.saldo < valor) {
             throw new IllegalArgumentException("Valor insuficiente");
@@ -45,6 +56,11 @@ public class ContaBancaria {
         this.depositar(valor);
         destino.depositar(valor);
     }
+
+    protected void incluirMovimento(Movimento m){
+        movimento.add(m);
+    }
+
 
 
 }
--- exercicio9/src/Questao3e4/Movimento.java
+++ exercicio9/src/Questao3e4/Movimento.java
@@ -1,6 +1,51 @@
 package Questao3e4;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 
 public class Movimento {
+
+    private double valor;
+    private TipoMovimento tipo;
+    private LocalDate data;
+    private LocalTime hora;
+
+    public Movimento(double valor, TipoMovimento tipo) {
+        this.valor = valor;
+        this.tipo = tipo;
+        this.data = LocalDate.now();
+        this.hora = LocalTime.now();
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public TipoMovimento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoMovimento tipo) {
+        this.tipo = tipo;
+    }
 }
